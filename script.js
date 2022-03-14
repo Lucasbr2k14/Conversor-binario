c7ff4ce5194dad7041ffefb919e4e57746985e45
Lucas <lucasbraubach@gmail.com>
2022-03-14 20:42:38 -0300

    Fixed

--- script.js
+++ script.js
@@ -16,34 +16,15 @@ function triger(value){
 
 function converterBin(value){
     
-    const number = parseInt(value)
-    const res = [{value:number, res:undefined}]
-    let result = ""
+    let num = value
+    let binary = (num % 2).toString()
 
-    while(res[res.length - 1].value > 1){
-
-        const dividend = res[res.length - 1].value
-
-        res.push({
-            value: dividend/2,
-            res: parseInt(dividend%2)
-        })
-
-
+    while(num > 1){
+        num = parseInt(num/2)
+        binary = (num % 2) + binary
     }
 
-    res.forEach((data, i) =>{
-        if(i > 0){
-        
-            if(result){
-                result += String(data.res)
-            }else{
-                result = String(data.res)
-            }
-        }
-    })
-    return result.split("").reverse().join("")
-
+    return binary
 }
 
 function converterInt(value){
